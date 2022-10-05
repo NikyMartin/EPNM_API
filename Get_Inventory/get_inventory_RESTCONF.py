4be8f6da557b59765425b89d115c1557a9cd1c42
NikyMartin <nicola.martino@gmail.com>
2022-10-05 09:41:35 +0200

    minor changes

--- Get_Inventory/get_inventory_RESTCONF.py
+++ Get_Inventory/get_inventory_RESTCONF.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 
 """
-    September 22nd, 2020
+    This script uses REST to extract following inventory details from EPN-M
+      - Interface Name
+      - Interface Description
+      - Module Type
+      - Part Number
+      - Serial Number
+      - Product ID
+
     Created by:
     Nicola Martino, nmartino@cisco.com
 
+    September 22nd, 2020
+      - First release
+
     October 03, 2022
-    Converted from using ET to JSON
+      - Converted from using ET to JSON
+
+    October 05, 2022
+      - Moved Common controls on a different file
+      - Added more controls
 
 """
 
@@ -15,51 +29,11 @@ import sys
 import getpass
 import json
 from tabulate import tabulate
-
 import urllib3
+
+from commonControls import isOpen, isEPNM, isAccountValid, sendGET, isNodeValid, getHeaders
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
-def getLastIndex(payload):
-    try:
-       lastIndex=json.loads(payload)['com.response-message']['com.header']['com.lastIndex']
-    except Exception as e:
-        exit(1)
-    return lastIndex
-
-
-def getHeaders():
-    headers = {
-        'Accept': 'application/json',
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/json',
-    }
-    return headers
-
-
-def sendGET(url,user,pwd):
-    response = requests.get(url, headers=getHeaders(), verify=False, auth=(user, pwd))
-    if response.status_code != 200:
-        error_message = json.loads(response._content.decode())["errorDocument"]["message"]
-        return False, response.status_code
-    else:
-        return True, response.text
-
-
-def isNodeValid(server_ip, user, pwd, node_name):
-    nd_fdn = 'MD=CISCO_EPNM!ND='+node_name
-    url = 'https://'+server_ip+'/restconf/data/v1/cisco-resource-physical:node?.depth=1&fdn='+nd_fdn
-    status, resp = sendGET(url,user,pwd)
-    if not status:
-        print("ERROR: It was not possible to run execute GET node")
-        print("Server returned :",resp.status_code)
-        exit(1)
-    else:
-        if getLastIndex(resp) == 0:
-            return True
-        else:
-            return False
-
 
 def getNode(server_ip, user, pwd, node_name):
     nd_fdn = 'MD=CISCO_EPNM!ND=' + node_name
@@ -83,42 +57,61 @@ if __name__ == '__main__':
     if len(sys.argv)!=3:
        print('\nMust pass device IP address and device name as script arguments\n')
        exit()
-    scripts, server_ip, device_name = sys.argv
+    scripts, server_ip, node_name = sys.argv
+
+# Check if https server is up and port 443 is reachable
+
+    if not isOpen(server_ip, 443):
+        print("\nERROR: " + server_ip + " is not reachable, either the server is down or port 443 is filtered\n")
+        exit()
+
+# Check if https server is EPNM
+
+    if not isEPNM(server_ip):
+        print("\nERROR: " + server_ip + " is not an EPN-M Server\n")
+        exit()
 
     username = input("Enter Device Username: ")
     password = getpass.getpass()
 
-    if isNodeValid(server_ip, username, password, device_name):
-        node = json.loads(getNode(server_ip, username, password, device_name))
+    if not isAccountValid(server_ip, username, password):
+        print("\nERROR: Check if username and password are correct\n")
+        exit()
+
+    if not isNodeValid(server_ip, username, password, node_name):
+        print("\nERROR: node", node_name, "not found\n")
+        exit()
+
+    node = json.loads(getNode(server_ip, username, password, node_name))
+
+    try:
+        equipment_list = node['com.response-message']['com.data']['nd.node'][0]['nd.equipment-list']['eq.equipment']
+    except:
+        print("Could not find equipment-list")
+        exit(1)
+
+    list = []
+    for equipment in equipment_list:
 
         try:
-            equipment_list = node['com.response-message']['com.data']['nd.node'][0]['nd.equipment-list']['eq.equipment']
+            part_number = equipment['eq.part-number']
         except:
-            print("Could not find equipment-list")
-            exit(1)
+            part_number = ""
 
-        list = []
-        for equipment in equipment_list:
+        try:
+            product_id = equipment['eq.product-id']
+        except:
+            product_id = ""
 
-            try:
-                part_number = equipment['eq.part-number']
-            except:
-                part_number = ""
+        try:
+            serial_number = equipment['eq.serial-number']
+        except:
+            serial_number = ""
 
-            try:
-                product_id = equipment['eq.product-id']
-            except:
-                product_id = ""
+        entry = (equipment['fdtn.name'], equipment['fdtn.description'], equipment['eq.equipment-type'],
+                 part_number, serial_number, product_id)
+        list.append(entry)
 
-            try:
-                serial_number = equipment['eq.serial-number']
-            except:
-                serial_number = ""
-
-            entry = (equipment['fdtn.name'], equipment['fdtn.description'], equipment['eq.equipment-type'],
-                     part_number, serial_number, product_id)
-            list.append(entry)
-
-        print(tabulate(list, headers=(['Name', 'Description', 'Type', 'PartNumber', 'SerialNumber', 'ProductID'])))
-    else:
-        print("\nNode", device_name, "not found\n")
+    print(tabulate(list, headers=(['Name', 'Description', 'Type', 'PartNumber', 'SerialNumber', 'ProductID'])))
+# output table might get ordered with
+#   print(tabulate(ordered(list), headers=(['Name', 'Description', 'Type', 'PartNumber', 'SerialNumber', 'ProductID'])))
